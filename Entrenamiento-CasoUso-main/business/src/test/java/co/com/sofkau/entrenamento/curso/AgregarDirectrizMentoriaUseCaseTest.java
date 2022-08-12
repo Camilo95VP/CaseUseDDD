4033e98548337d1350537b9476159846a175fa27
JuanC <camiloflstudio@gmail.com>
2022-08-12 17:36:05 -0500

    Se implementa la segunda A = Actuar

--- Entrenamiento-CasoUso-main/business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
+++ Entrenamiento-CasoUso-main/business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -1,16 +1,25 @@
 package co.com.sofkau.entrenamento.curso;
 
 
+import co.com.sofka.business.generic.UseCaseHandler;
 import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
 import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
+import co.com.sofkau.entrenamiento.curso.events.MentoriaCreada;
 import co.com.sofkau.entrenamiento.curso.values.CursoId;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -23,12 +32,30 @@ public class AgregarDirectrizMentoriaUseCaseTest {
 
     @Test
     void agregarDirectrizAMentoria() {
+
         //arrange
         CursoId cursoId = CursoId.of("curso");
         MentoriaId mentoriaId = MentoriaId.of("ment");
         Directiz directiz = new Directiz("xdxdxd");
         var command = new AgregarDirectrizMentoria(cursoId,mentoriaId,directiz);
 
+        when(repository.getEventsBy("curso")).thenReturn(history());
+        useCase.addRepository(repository);
+
+
+        //act
+        var events = UseCaseHandler.getInstance()
+                .setIdentifyExecutor(command.getMentoriaId().value())
+                .syncExecutor(useCase, new RequestCommand<>(command))
+                .orElseThrow()
+                .getDomainEvents();
+
+
+
+
+    }
+
+    private List<DomainEvent> history() {
     }
 }
 
