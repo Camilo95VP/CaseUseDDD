0e333eab33716e98c8c707852b48943d164eccdb
JuanC <camiloflstudio@gmail.com>
2022-08-12 13:26:50 -0500

    Se implementa el ARRANGE pare el TEST

--- Entrenamiento-CasoUso-main/business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
+++ Entrenamiento-CasoUso-main/business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -2,6 +2,10 @@ package co.com.sofkau.entrenamento.curso;
 
 
 import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
+import co.com.sofkau.entrenamiento.curso.values.Directiz;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,6 +23,11 @@ public class AgregarDirectrizMentoriaUseCaseTest {
 
     @Test
     void agregarDirectrizAMentoria() {
+        //arrange
+        CursoId cursoId = CursoId.of("curso");
+        MentoriaId mentoriaId = MentoriaId.of("ment");
+        Directiz directiz = new Directiz("xdxdxd");
+        var command = new AgregarDirectrizMentoria(cursoId,mentoriaId,directiz);
 
     }
 }
