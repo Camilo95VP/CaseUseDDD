024dcaf474744760c09d2604964d9ae32a3c88af
JuanC <camiloflstudio@gmail.com>
2022-08-12 22:21:29 -0500

    Creacion de history, curso y directriz en una lista de domain event

--- Entrenamiento-CasoUso-main/business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
+++ Entrenamiento-CasoUso-main/business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -5,12 +5,17 @@ import co.com.sofka.business.generic.UseCaseHandler;
 import co.com.sofka.business.repository.DomainEventRepository;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.entrenamiento.curso.Mentoria;
 import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
+import co.com.sofkau.entrenamiento.curso.events.CursoCreado;
 import co.com.sofkau.entrenamiento.curso.events.DirectrizAgregadaAMentoria;
 import co.com.sofkau.entrenamiento.curso.events.MentoriaCreada;
 import co.com.sofkau.entrenamiento.curso.values.CursoId;
+import co.com.sofkau.entrenamiento.curso.values.Descripcion;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
+import co.com.sofkau.entrenamiento.curso.values.Fecha;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+import co.com.sofkau.entrenamiento.curso.values.Nombre;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +23,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -35,14 +42,13 @@ public class AgregarDirectrizMentoriaUseCaseTest {
     void agregarDirectrizAMentoria() {
 
         //arrange
-        CursoId cursoId = CursoId.of("curso");
-        MentoriaId mentoriaId = MentoriaId.of("ment");
-        Directiz directiz = new Directiz("xdxdxd");
+        CursoId cursoId = CursoId.of("Sofka DDD");
+        MentoriaId mentoriaId = MentoriaId.of("Implementando de Test");
+        Directiz directiz = new Directiz("Training");
         var command = new AgregarDirectrizMentoria(cursoId,mentoriaId,directiz);
 
-        when(repository.getEventsBy("curso")).thenReturn(history());
+        when(repository.getEventsBy("Sofka DDD")).thenReturn(history());
         useCase.addRepository(repository);
-
 
         //act
         var events = UseCaseHandler.getInstance()
@@ -51,14 +57,19 @@ public class AgregarDirectrizMentoriaUseCaseTest {
                 .orElseThrow()
                 .getDomainEvents();
 
-
         //assert
         var event = (DirectrizAgregadaAMentoria)events.get(0);
-        Assertions.assertEquals("xdxdxd", event.getDirectiz().value());
-
+        Assertions.assertEquals("Training", event.getDirectiz().value());
     }
 
     private List<DomainEvent> history() {
+
+        var nombreCurso = new Nombre("SofkaU");
+        var descripcionCurso = new Descripcion("Training");
+        var cursoid = CursoId.of("123456");
+        var event = new CursoCreado(nombreCurso,descripcionCurso);
+        event.setAggregateRootId(cursoid.value());
+        return List.of(event);
     }
 }
 
